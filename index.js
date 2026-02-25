fa2f7e5a93207dd96015f48884f7f8a7816f2176
Eduardo Gabriel Kenzo Tanaka <eduduardogabriel888@gmail.com>
2026-02-24 22:32:22 -0300

    basic api functionality

--- index.js
+++ index.js
@@ -12,12 +12,27 @@ const limiter = rateLimit({
 });
 const apiUrl = "https://byabbe.se/on-this-day/";
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(limiter);
-
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/search", async (req, res) => {
+  const month = req.body.month;
+  const day = req.body.day;
+  try {
+    const result = await axios.get(apiUrl + `${month}/${day}/events.json`);
+    res.render("index.ejs", {
+      date: result.data.date,
+      events: result.data.events,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.render("index.ejs", { error: error });
+  }
 });
 
 app.listen(port, () => {
